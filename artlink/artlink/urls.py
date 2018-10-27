e7b27bbcfba6dfc11bc920114d76bd8e0de9254c
SamJGeo <sam_g.1@hotmail.co.uk>
2018-10-27 20:16:24 +0100

    population changes

--- artlink/artlink/urls.py
+++ artlink/artlink/urls.py
@@ -4,10 +4,10 @@ from django.conf.urls import include
 from artlink import views
 
 urlpatterns = [
+	url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^activity/(?P<activity_slug>[\w\-]+)/$', views.activity_show, name='activity_show'),
     url(r'^$', views.add, name='add'),
     url(r'^$', views.browse, name='browse'),
     url(r'^$', views.about, name='about'),
-    url(r'^admin/', admin.site.urls),
 ]
--- artlink/population_script.py
+++ artlink/population_script.py
@@ -1,12 +1,14 @@
 # Populate local database with sample data
 import json
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","artlink_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","artlink.settings")
 
 import django
 django.setup()
 
-from artlink.models import *
+from cfg.models import *
+
+
 
 def populate():
 
@@ -14,6 +16,13 @@ def populate():
     with open("population_data.json", "r") as f:
         activities = json.loads("".join(f.readlines()))
     # Populate database with data
+
+
+    
+    senses = ["Sound","Smell","Sight","Touch","Taste"]
+    for s in senses:
+        add_senses(s)
+
     for a in activities:
         add_activity(a)
 
@@ -22,11 +31,17 @@ def populate():
 
 def add_activity(activity):
     # This will throw a RuntimeWarning during population, which can be ignored for now.
-    a = activity.objects.get_or_create(title=activity["title"],description = activity["description"], features= activity["features"], place = activity["place"], cost= activity["cost"], contact= activity["contact"], welcomeComment= activity["welcomeComment"], webLink = activity["webLink"],review = review.objects.get(review =activity["review"]) ,description = activity["description"] ,sense=sense.objects.get(sense=activity["sense"]))[0]
+    a = Activity.objects.get_or_create(title=activity["title"],description = activity["description"], features= activity["features"], place = activity["place"], cost= activity["cost"], contact= activity["contact"], welcomeComment= activity["welcomeComment"], webLink = activity["webLink"],review = review.objects.get(review =activity["review"]) ,description = activity["description"] ,sense=sense.objects.get(sense=activity["sense"]))[0]
 
     a.save()
     print("Activity added: %s" % e.title)
 
+
+def add_senses(sense):
+    s = Sense.objects.get_or_create(sense = sense)
+    s.save()
+    print("Sense added %s" % s.sense)
+    
 
 if __name__ == "__main__":
     print("Starting population script for artlink")
