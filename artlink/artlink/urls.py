60b015f5c64b0cd12a46c83fda829a7deffcd73e
Leonore Papaloizos <2264897V@student.gla.ac.uk>
2018-10-27 22:49:26 +0100

    coke-powered url changes

--- artlink/artlink/urls.py
+++ artlink/artlink/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls import include
 from artlink import views
 
 urlpatterns = [
-	url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^activity/(?P<activity_slug>[\w\-]+)/$', views.activity_show, name='activity_show'),
     url(r'^add$', views.add, name='add'),
