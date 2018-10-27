030968a17b465bba9638ab334aeb35444e45c9f0
Leonore Papaloizos <2264897V@student.gla.ac.uk>
2018-10-27 21:52:28 +0100

    some bugs are gone some aren't coffee has been consumed

--- artlink/artlink/models.py
+++ artlink/artlink/models.py
@@ -6,28 +6,28 @@ from django.template.defaultfilters import slugify
 
 
 class Review(models.Model):
-    review =  models.CharField(max_length=10, unique= True)
-    name = models.CharField(max_length=128, unique= True)
+    review =  models.TextField(blank=True)
+    name = models.CharField(max_length=60, unique= True)
 
     def __str__(self):
         return self.name
 
-        
+
 class Sense(models.Model):
     sense = models.CharField(max_length=10, unique= True)
 
 
 class Activity(models.Model):
     title = models.CharField(max_length=100, unique=True)
-    features = models.CharField(max_length=128, unique=True)
+    features = models.TextField()
     place = models.CharField(max_length=128, unique=True)
     cost = models.CharField(max_length=50, unique=True)
     contact = models.CharField(max_length=128, unique=True)
-    welcomeComment = models.CharField(max_length=400, unique=True)
+    welcomeComment = models.TextField(blank=True)
     webLink = models.CharField(max_length=128, unique=True)
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
     sense = models.ForeignKey(Sense, on_delete=models.CASCADE)
-    description = models.CharField(max_length=400, unique=True)
+    description = models.TextField()
 
     slug = models.SlugField(unique=True)
 
