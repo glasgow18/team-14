3f4b0f8578a6b7b0fdb932b10e8857f2b619a776
Nico Morell <nicomorell@outlook.com>
2018-10-27 14:21:51 +0100

    Fixes URL

--- artlink/cfg/views.py
+++ artlink/cfg/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    return HttpResponse("Hello World!")
